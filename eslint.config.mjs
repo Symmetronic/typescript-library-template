cd7c693e90de028eca9a0acc3dfbc9363d11bedc
Simon Hochholzer <simon@symmetronic.com>
2025-03-11 17:04:54 +0100

    chore(deps-dev): add eslint-plugin-import

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,11 @@
 import eslint from "@eslint/js";
+import importPlugin from "eslint-plugin-import";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
+  importPlugin.flatConfigs.recommended,
+  importPlugin.flatConfigs.typescript,
   tseslint.configs.recommendedTypeChecked,
   tseslint.configs.stylisticTypeChecked,
   {
@@ -11,6 +14,12 @@ export default tseslint.config(
       parserOptions: {
         projectService: true,
         tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    settings: {
+      "import/resolver": {
+        typescript: true,
+        node: true,
       },
     },
   },
