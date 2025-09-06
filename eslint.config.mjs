2a4aa0028deec8374fc6fb4e13a4acfcec7b3578
Simon <simon@symmetronic.com>
2025-09-06 14:39:34 +0200

    chore: improve eslint config (#115)

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,32 @@
+// @ts-check
+
 import eslint from "@eslint/js";
 import importPlugin from "eslint-plugin-import";
+import { globalIgnores } from "eslint/config";
 import tseslint from "typescript-eslint";
 
 export default tseslint.config(
   eslint.configs.recommended,
-  importPlugin.flatConfigs.recommended,
-  importPlugin.flatConfigs.typescript,
-  tseslint.configs.recommendedTypeChecked,
-  tseslint.configs.stylisticTypeChecked,
+  tseslint.configs.recommended,
   {
-    ignores: [
-      ".cache",
-      "coverage",
-      "dist",
-      "eslint.config.mjs",
-      "node_modules",
-      "prettier.config.mjs",
-      "vite.config.ts",
-    ],
-    languageOptions: {
-      parserOptions: {
-        projectService: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
     settings: {
       "import/resolver": {
         typescript: true,
         node: true,
       },
     },
+  },
+  globalIgnores([
+    ".cache/**/*",
+    "coverage/**/*",
+    "dist/**/*",
+    "node_modules/**/*",
+  ]),
+  {
+    files: ["**/*.{ts,tsx}"],
+    extends: [
+      importPlugin.flatConfigs.recommended,
+      importPlugin.flatConfigs.typescript,
+    ],
   },
 );
